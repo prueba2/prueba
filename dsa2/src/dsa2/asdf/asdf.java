bf06c147b2deeca3ebb4e1ec6f9d41a14c20694b
alberto <alberto.cruz.gines@gmail.com>
2012-10-26 10:37:35 +0200

    modificado

--- dsa2/src/dsa2/asdf/asdf.java
+++ dsa2/src/dsa2/asdf/asdf.java
@@ -7,6 +7,12 @@ public class asdf {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		jugador();
+		
+	}
+	
+	public static void jugador() {
+		System.out.println("Hello jugador");
 	}
 
 }
