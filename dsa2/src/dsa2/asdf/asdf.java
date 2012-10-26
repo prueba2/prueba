1934acd23bba92026806c5294a6152d5876221e9
alberto <alberto.cruz.gines@gmail.com>
2012-10-26 10:45:52 +0200

    Solucionado merge1

---
+++ dsa2/src/dsa2/asdf/asdf.java
@@ -0,0 +1,23 @@
+package dsa2.asdf;
+
+public class asdf {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		main2();
+		jugador();
+		
+	}
+	
+	public static void jugador() {
+		System.out.println("Hello jugador");
+	}
+	
+	public static void main2() {
+		System.out.println("Hello world angel");
+	}
+
+}
