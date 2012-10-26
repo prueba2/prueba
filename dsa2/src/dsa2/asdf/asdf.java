8e56c7ddc5ccf58f7ad5083aae9400aff83edb74
Angel <Angel@10.189.32.110>
2012-10-26 10:37:52 +0200

    main 2

--- dsa2/src/dsa2/asdf/asdf.java
+++ dsa2/src/dsa2/asdf/asdf.java
@@ -7,6 +7,11 @@ public class asdf {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		main2();
+	}
+	
+	public static void main2() {
+		System.out.println("Hello world angel");
 	}
 
 }
